7cc4d17aed67235c1bed2cef7ceb9d0dbab30388
Nagesh05221981 <allur_nagesh@yahoo.co.in>
2025-11-23 21:07:21 -0600

    Initial Commit

---
+++ LoginAppMVVM/LoginAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppMVVMApp.swift
+//  LoginAppMVVM
+//
+//  Created by Nagesh Allur on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
